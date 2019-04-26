065807f7891c4e408ff7a2bc65be2b9507f358e2
Youmna-abdalaziz <youmnaabdalaziz@gmail.com>
2019-04-26 17:01:20 +0200

    merge

--- ecommerce/db/schema.rb
+++ ecommerce/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_26_104229) do
+ActiveRecord::Schema.define(version: 2019_04_26_104926) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -61,6 +61,9 @@ ActiveRecord::Schema.define(version: 2019_04_26_104229) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
+    t.text "image"
+    t.float "price"
+    t.integer "quantity_in_stock"
     t.text "description"
     t.integer "category_id"
     t.integer "brand_id"
@@ -74,6 +77,7 @@ ActiveRecord::Schema.define(version: 2019_04_26_104229) do
 
   create_table "stores", force: :cascade do |t|
     t.string "name"
+    t.text "summary"
     t.integer "seller_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
