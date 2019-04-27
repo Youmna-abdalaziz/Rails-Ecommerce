61998ddf409cf1ec77072212274b1f91bd4e7283
eman <eman.zayan95@gmail.com>
2019-04-27 17:45:24 +0200

    add user-cpouons table

--- ecommerce/app/models/coupon.rb
+++ ecommerce/app/models/coupon.rb
@@ -1,3 +1,3 @@
 class Coupon < ApplicationRecord
-    has_many:users , dependent: :destroy
+    has_and_belongs_to_many :users
 end
--- ecommerce/db/schema.rb
+++ ecommerce/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_26_185952) do
+ActiveRecord::Schema.define(version: 2019_04_27_154117) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -117,6 +117,11 @@ ActiveRecord::Schema.define(version: 2019_04_26_185952) do
     t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "users_coupons", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "coupon_id"
   end
 
 end
