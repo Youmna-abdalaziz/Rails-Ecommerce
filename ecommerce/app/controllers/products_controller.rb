c1f42c5f2a68109653e86395fcd556e267aa139c
faten-ibrahim <faten_elssayad@yahoo.com>
2019-04-27 10:17:12 +0200

    add image with each  product and view it

--- ecommerce/app/controllers/products_controller.rb
+++ ecommerce/app/controllers/products_controller.rb
@@ -1,9 +1,27 @@
 class ProductsController < InheritedResources::Base
 
+
+  # def new
+  #   @category = Category.all
+  #   @brand = Brand.all
+  #   @store = Store.all
+  #   # authorize! :create, @article    
+  # end
+
+  # def edit
+  #   @category = Category.all
+  #   @brand = Brand.all
+  #   @store = Store.all
+  #   # authorize! :update, @category
+   
+  # end
+  
   private
 
     def product_params
       params.require(:product).permit(:title, :image, :price, :quantity_in_stock, :description, :category_id, :brand_id, :store_id)
     end
+
+    
 
 end
--- ecommerce/app/models/product.rb
+++ ecommerce/app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   belongs_to :category
   belongs_to :brand
   belongs_to :store
+  has_one_attached :image
 
   def product_category
     category.name
