f0a5a05dbd2eed67228637feb93043f6d0da7b8a
faten-ibrahim <faten_elssayad@yahoo.com>
2019-04-28 17:08:14 +0200

    add users roles and write ability file for every role authorization

--- ecommerce/app/controllers/stores_controller.rb
+++ ecommerce/app/controllers/stores_controller.rb
@@ -1,4 +1,17 @@
 class StoresController < InheritedResources::Base
+  before_action :authenticate_user!, except: [:index]
+  # include CanCan::ControllerAdditions
+  load_and_authorize_resource
+
+  def index
+    @store=Store.all
+    authorize! :read, @store
+  
+  end 
+  def new
+    @store =Store.new
+    authorize! :create, @store
+  end
 
   private
 
--- ecommerce/app/models/user.rb
+++ ecommerce/app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
  # mount_uploader :avatar, AvatarUploader
   has_and_belongs_to_many :coupons
   has_one_attached :avatar
+
+  def is_seller?
+       is_seller
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
