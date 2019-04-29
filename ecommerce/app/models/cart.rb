8a6e0401e0bff642a9b2c68e47cba440e97d0774
Narmeen-adel <memaadel24@gmail.com>
2019-04-29 16:03:27 +0200

    merge

--- ecommerce/app/models/cart.rb
+++ ecommerce/app/models/cart.rb
@@ -3,4 +3,11 @@ class Cart < ApplicationRecord
   belongs_to :product
   validates :quantity, presence: true
   validates :product_id, presence: true
+
+  def cart_product
+    product.title
+  end
+  def cart_user
+    user.name
+  end
 end
