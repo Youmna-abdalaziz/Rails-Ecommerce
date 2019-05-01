b713ae4b9be98c37641954c921ab43050c96ba6f
Youmna-abdalaziz <youmnaabdalaziz@gmail.com>
2019-05-01 22:24:44 +0200

    Merge

--- ecommerce/db/migrate/20190430202640_remove_d_price_from_coupons.rb
+++
@@ -1,5 +0,0 @@
-class RemoveDPriceFromCoupons < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :coupons, :Dprice, :integer
-  end
-end
--- ecommerce/db/schema.rb
+++ ecommerce/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_30_203031) do
+ActiveRecord::Schema.define(version: 2019_05_01_130706) do
 
   create_table "active_admin_comments", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "namespace"
@@ -83,7 +83,7 @@ ActiveRecord::Schema.define(version: 2019_04_30_203031) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "coupons", force: :cascade do |t|
+  create_table "coupons", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "expiring_type"
