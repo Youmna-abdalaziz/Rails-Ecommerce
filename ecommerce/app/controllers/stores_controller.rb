52296d39bfa2a7a5b17021386f4115fc09950520
faten-ibrahim <faten_elssayad@yahoo.com>
2019-05-02 15:05:42 +0200

    edit store controller to create stores

--- ecommerce/app/controllers/stores_controller.rb
+++ ecommerce/app/controllers/stores_controller.rb
@@ -1,16 +1,29 @@
 class StoresController < InheritedResources::Base
   before_action :authenticate_user!, except: [:index]
   # include CanCan::ControllerAdditions
-  load_and_authorize_resource
+  # load_and_authorize_resource
 
   def index
     @store=Store.all
-    authorize! :read, @store
+    # authorize! :read, @store
   
   end 
   def new
     @store =Store.new
-    authorize! :create, @store
+    # authorize! :create, @store
+  end
+
+  def create
+    @store = Store.new(store_params)
+    puts "******************* #{store_params}*********"
+    # @product.user_id = current_user.id
+
+    if @store.save
+   
+      redirect_to @store
+    else
+        render 'new'
+    end
   end
 
   private
