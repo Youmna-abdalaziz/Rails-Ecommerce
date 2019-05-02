734400f817255b17be9fe03eeda45494491809c1
eman <eman.zayan95@gmail.com>
2019-05-02 16:23:47 +0200

    add filter with seller and with price range

--- ecommerce/app/models/product.rb
+++ ecommerce/app/models/product.rb
@@ -30,5 +30,12 @@ class Product < ApplicationRecord
 
     # where("brand_id LIKE ?","#{filter_brand}")
   end
-  
+  def self.filters(filter_seller,search_term)
+    @Product=Product.where("LOWER(title) LIKE :search_term OR LOWER(description) LIKE :search_term ",search_term: "%#{search_term.downcase}").where("user_id LIKE ?","#{filter_seller}")
+
+    # where("brand_id LIKE ?","#{filter_brand}")
+  end
+  def self.filterp(price,tprice,search_term)
+    @Product=Product.where("LOWER(title) LIKE :search_term OR LOWER(description) LIKE :search_term ",search_term: "%#{search_term.downcase}").where("price > ? AND price < ? " , "#{price.to_i}","#{tprice.to_i}")
+  end
 end
