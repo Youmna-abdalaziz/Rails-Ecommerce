7418e805f9652568b28708c9980c65ce0b2774e4
Youmna-abdalaziz <youmnaabdalaziz@gmail.com>
2019-05-04 00:52:38 +0200

    Add title to coupon form and index

--- ecommerce/app/controllers/coupons_controller.rb
+++ ecommerce/app/controllers/coupons_controller.rb
@@ -3,7 +3,7 @@ class CouponsController < InheritedResources::Base
   private
 
     def coupon_params
-      params.require(:coupon).permit(:discount_Type, :discount_value, :usage_num,:expiring_date,:expiring_type)
+      params.require(:coupon).permit(:discount_Type, :discount_value, :usage_num,:expiring_date,:expiring_type, :title)
     end
 
 end
