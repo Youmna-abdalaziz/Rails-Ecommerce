f8960272c201998aab546b1786e96c83377aca6b
Youmna-abdalaziz <youmnaabdalaziz@gmail.com>
2019-05-04 00:56:53 +0200

    Add validation on coupon fields

--- ecommerce/app/models/coupon.rb
+++ ecommerce/app/models/coupon.rb
@@ -1,6 +1,14 @@
 class Coupon < ApplicationRecord
     has_and_belongs_to_many :users
     has_many :products
+    validates :title , presence:true, on: :create
+    validates :expiring_type , presence:true, on: :create
+    validates :discount_Type , presence:true, on: :create
+    validates :discount_value , presence:true, on: :create
+
+
+
+
 
     def validate_coupon
         if self.expiring_type == "date"
