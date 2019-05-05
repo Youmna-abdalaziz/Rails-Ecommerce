de4ddff88bbbc5ec2d57ec6cc2b0a43d4a0fb893
Youmna-abdalaziz <youmnaabdalaziz@gmail.com>
2019-05-05 21:24:19 +0200

    Fix new order bug

--- ecommerce/app/controllers/orders_controller.rb
+++ ecommerce/app/controllers/orders_controller.rb
@@ -1,74 +1,94 @@
-class CartsController < ApplicationController
+class OrdersController < InheritedResources::Base
+  # before_action :validates, only: [:new,:create]
+
   before_action :authenticate_user!
-  
-  before_action :set_product, only: [:new,:create]
-      def new 
-        @cart=Cart.new
-        create
-      end 
-      def index
-          @carts = current_user.carts
-          calculate_total
-      end
-      def edit
-          @cart = Cart.find(params[:id])
-        
-      end
-      def update
-        @cart = Cart.find(params[:id])
-        if (cart_params[:quantity].to_i >= 1.to_i) && (@cart.product.quantity_in_stock.to_i >= cart_params[:quantity].to_i)
-          @cart.quantity = cart_params[:quantity].to_i
-          @cart.total = @cart.quantity * @cart.unit_price
-          @cart.save
-        end
 
-        redirect_to carts_path()
+   def show
+       @order = Order.find(params[:id])
+   end
+  def new
+      @order = Order.new
+    end
+   def index
+      @orders = current_user.orders
+      #Order.all
+   end
+#   def edit
+#       @orders = Order.find(params[:id])
+#   end
+#   def update
+#       @order = Order.find(params[:id])
+    
+#       if @order.update(order_params)
+#         redirect_to @article
+#       else
+#         render 'edit'
+#       end
+#   end
+
+ #render plain: params[:article].inspect
+      #render plain: article_params
+   def create
+    # flash.now[:error] = "Your cart is empty   ."  
+    @order = Order.new(order_params)
+    @order.user=current_user
+    
+
+    carts =current_user.carts
+    validate =0
+    errors=[]
+    if carts.count == 0
+      errors.push("cart is empty")
+    end
+       
+    carts.each do|item|
+      if item.quantity.to_i > item.product.quantity_in_stock.to_i
+          errors.push("product #{item.product.title} not available in stock with Quqntity #{item.quantity} " )
       end
-     
-      def create
-          if current_user.carts.find_by(:product_id => @product)
-            @cart = current_user.carts.find_by(:product_id => @product)
-            if @product.quantity_in_stock.to_i + 1 >= @cart.quantity
-              @cart.quantity += 1
-              @cart.unit_price=@product.price
-              @cart.total =@product.price * @cart.quantity 
+    end
+    @order.valid?
+     if errors.count == 0
+         @order.status="pending"
+         @order.total_price = @order.actual_price = 0 
+         if @order.save 
+            total_price = 0
+            actual_price = 0 
+            carts.each do|item|
+              #######################
+                      @order_product=OrderProduct.new
+                      @order_product.order_id = @order.id
+                      @order_product.product = item.product
+                      @order_product.quantity =item.quantity
+                      @order_product.unit_price =item.unit_price
+                      @order_product.status="pending"
+                      @order_product.calculate_actual_price
+                      @order_product.save
+                      ############ after save order _product 
+                      total_price += @order_product.quantity * @order_product.unit_price
+                      actual_price += @order_product.actual_price.to_i
+              end
+              @order.actual_price = actual_price
+              @order.total_price = total_price
+              @order.save
+              current_user.carts.delete_all
+              redirect_to @order
+            else
+              render 'new'
             end
-          else
-            @cart = Cart.new
-            @cart.user = current_user
-            @cart.product = @product
-            @cart.quantity=1
-            @cart.unit_price=@product.price
-            @cart.total =@product.price
-          end
-          @cart.save
-          redirect_to carts_path()
-            
+      
+      else 
+        flash[:error] = "Your cart is empty or in valid check your cart !!!  ."  
+        render 'new'             
       end
-      def destroy
-          @cart = Cart.find(params[:id])
-          @cart.destroy
-          redirect_to carts_path()
-      end
-      private
-        def cart_params
-          params.require(:cart).permit(:quantity)
-        end
-        def set_product
-          @product =Product.find(params[:product_id])
-        end
-        def set_user
-          @user = current_user
-        end
 
-        def calculate_total
-          @total=0
-          current_user.carts.each do|item|
-            @total += item.total
-          end
-          @total
-        end
+    end
+  
 
+  private
+
+    def order_params
+      params.require(:order).permit(:address,:credit_cart,:phone)
+      
+    end
 
 end
-
