b46f36254d55102eeb5133872d3999b0ab2c7955
Youmna-abdalaziz <youmnaabdalaziz@gmail.com>
2019-05-06 21:59:18 +0200

    Return the coupon form with selectors

--- ecommerce/app/admin/coupons.rb
+++ ecommerce/app/admin/coupons.rb
@@ -1,3 +1,16 @@
 ActiveAdmin.register Coupon do
-    permit_params :expireddate, :Dprice, :precentage, :usagenum
+    permit_params :title, :expiring_date , :usage_num, :discount_Type, :discount_value, :expiring_type
+
+
+    form do |f|
+        f.inputs "coupon" do 
+            f.input :title, :label => "Title"
+            f.input :expiring_date ,:lable =>"expiring_date"
+            f.input :usage_num ,:lable =>"usage_num"
+            f.input :discount_Type ,:lable =>"Discount Type" , :as => :select, :collection => [["Fixed" , "Fixed"],["Percentage","Percentage"]]
+            f.input :discount_value ,:lable =>"discount_value"
+            f.input :expiring_type ,:lable =>"Expiring Type" , :as => :select, :collection => [["usage_num","usage_num"],["expiring_date","expiring_date"],["Both","Both"]]
+            f.actions
+        end
+    end
 end
--- ecommerce/app/admin/stores.rb
+++ ecommerce/app/admin/stores.rb
@@ -1,24 +1,6 @@
 ActiveAdmin.register Store do
     permit_params :name , :summary, :user_id
 
-    # controller do
-    #     def create
-    #         store = Store.new(store_params)
-    #         puts "******************* #{store_params}*********"
-    #         # @product.user_id = current_user.id
-    #         _id=@store.user_id
-    #         def set_is_seller(_id)
-    #           a = User.find_by_id(_id)
-    #           a.is_seller = true
-    #           a.save
-    #         end  
-
-    #         set_is_seller(_id)
-    #         flash[:notice] = 'Store created successfully'
-      
-    #     end
-    #   end
-
         controller do
             def create
                 @store = Store.new(permitted_params[:store])
