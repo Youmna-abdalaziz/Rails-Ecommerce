c7af43b30f72c2d6bf6bf6937b6f0111aa61cf75
Youmna-abdalaziz <youmnaabdalaziz@gmail.com>
2019-05-12 00:48:03 +0200

    Add seeds data for products

--- ecommerce/db/seeds.rb
+++ ecommerce/db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
-User.create(email: "faten@yahoo.com",password: '123456',name:"faten",is_seller: true,id:7)
-User.create(email: "soha@yahoo.com",password: '123456',name:"soha",is_seller: true,id:8)
-User.create(email: "ali@yahoo.com",password: '123456',name:"ali")
-User.create(email: "ahmed@yahoo.com",password: '123456',name:"ahmed")
+User.create(email: "faten@yahoo.com",password: '123456',password_confirmation:'123456', name:"faten",is_seller: true,id:7)
+User.create(email: "soha@yahoo.com",password: '123456',password_confirmation:'123456', name:"soha",is_seller: true,id:8)
+User.create(email: "ali@yahoo.com",password: '123456',password_confirmation:'123456',name:"ali")
+User.create(email: "ahmed@yahoo.com",password: '123456',password_confirmation:'123456',name:"ahmed")
 
 Brand.create(name: "Samsung")
 Brand.create(name: "htc")
@@ -31,23 +31,25 @@ Category.create(name: "Makeup")
 Category.create(name: "Media")
 Category.create(name: "Apparel")
 Category.create(name: "Books")
-Store.create(name:"store1",summary:"store1",user_id:1)
-Store.create(name:"store2",summary:"store2",user_id:2)
-Coupon.create(id:11,expiring_type: 'Both',discount_value: 12,discount_Type: 'Fixed',current_usage: 2, usage_num: 12, expiring_date:'2019-07-06 04:00:00', title:'c1')
-Coupon.create(id:24,expiring_type: 'expiring_date',discount_value: 12,discount_Type: 'Percentage',current_usage: 0,usage_num: 12, expiring_date:'2019-05-13 02:02:00',title: 'ccc')
-Coupon.create(id:25,expiring_type: 'usage_num',discount_value: 10,discount_Type: 'Fixed',current_usage: 1,usage_num: 10,expiring_date: '2019-06-06 06:05:00',title: 'test')
+Store.create(name:"store1",summary:"store1",user_id:7)
+Store.create(name:"store2",summary:"store2",user_id:8)
+Coupon.create(id:53,expiring_type: 'Both',discount_value: 12,discount_Type: 'Fixed',current_usage: 2, usage_num: 12, expiring_date:'2019-07-06 04:00:00', title:'c1')
+Coupon.create(id:54,expiring_type: 'expiring_date',discount_value: 12,discount_Type: 'Percentage',current_usage: 0,usage_num: 12, expiring_date:'2019-05-13 02:02:00',title: 'ccc')
+Coupon.create(id:55,expiring_type: 'usage_num',discount_value: 10,discount_Type: 'Fixed',current_usage: 1,usage_num: 10,expiring_date: '2019-06-06 06:05:00',title: 'test')
 
   
 # Store.create(name:"first store",summary:"We recommend Monty Python and the Holy Grail for some light-humoured dummy text.")
 # Store.create(name:"Second Store",summary:"Get ready for plenty of religious references that fail to make sense – Monty Python fans know what we mean")
 
- product1=Product.create(title:"Projector",image:"",price:1000,quantity_in_stock:100,description:"ready for plenty of religious references",category_id:2, brand_id:2,user_id:7)  
- product2=Product.create(title:"Routers",image:"",price:800,quantity_in_stock:60,description:"ready for plenty of religious references",category_id:3, brand_id:5,user_id:8)  
- product3=Product.create(title:"Printer",image:"",price:1000,quantity_in_stock:100,description:"ready for plenty of religious references",category_id:4, brand_id:3,user_id:7)  
-#product1.image.attach(io: File.open("app/assets/images/myavatar.png"), filename: "myavatar.png")
-#product2.image.attach(io: File.open("app/assets/images/avatar_nick.png"), filename: "avatar_nick.png")
-#product3.image.attach(io: File.open("app/assets/images/defaultFemalePP.png"), filename: "defaultFemalePP.png")
-
+ product1=Product.new(title:"Projector",price:1000,quantity_in_stock:100,description:"ready for plenty of religious references",category_id:2, brand_id:2,user_id:7)  
+ product1.image.attach(io: File.open("app/assets/images/projector.jpg"), filename: "projector.jpg")
+ product1.save!
+ product2=Product.new(title:"Routers",price:800,quantity_in_stock:60,description:"ready for plenty of religious references",category_id:3, brand_id:5,user_id:8)  
+ product2.image.attach(io: File.open("app/assets/images/router.jpg"), filename: "router.jpg")
+ product2.save!
+ product3=Product.new(title:"Printer",price:1000,quantity_in_stock:100,description:"ready for plenty of religious references",category_id:4, brand_id:3,user_id:7)  
+ product3.image.attach(io: File.open("app/assets/images/printer.jpg"), filename: "printer.jpg")
+ product3.save!
 
 
 
