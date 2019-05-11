63550ce2f4bb2d1d65f4eb5f216eab9d1eddfafc
Youmna-abdalaziz <youmnaabdalaziz@gmail.com>
2019-05-11 23:14:37 +0200

    Add seeds data for coupons and stores

--- ecommerce/config/environments/development.rb
+++ ecommerce/config/environments/development.rb
@@ -61,4 +61,5 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.active_job.queue_adapter = :inline
 end
--- ecommerce/db/seeds.rb
+++ ecommerce/db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
-User.create(email: "faten@yahoo.com",password: '123456',name:"faten",is_seller: true)
-User.create(email: "soha@yahoo.com",password: '123456',name:"soha",is_seller: true)
+User.create(email: "faten@yahoo.com",password: '123456',name:"faten",is_seller: true,id:7)
+User.create(email: "soha@yahoo.com",password: '123456',name:"soha",is_seller: true,id:8)
 User.create(email: "ali@yahoo.com",password: '123456',name:"ali")
 User.create(email: "ahmed@yahoo.com",password: '123456',name:"ahmed")
 
@@ -31,11 +31,33 @@ Category.create(name: "Makeup")
 Category.create(name: "Media")
 Category.create(name: "Apparel")
 Category.create(name: "Books")
+Store.create(name:"store1",summary:"store1",user_id:1)
+Store.create(name:"store2",summary:"store2",user_id:2)
+Coupon.create(id:1,expiring_date: 'Both',discount_value: 12,discount_Type: 'Fixed',current_usage: 2, usage_num: 12, expiring_date:'2019-07-06 04:00:00', title:'c1'),
+Coupon.create(id:2,expiring_type: 'expiring_date',discount_value: 15,discount_Type: 'Percentage',current_usage: 2,usage_num: NULL, expiring_date:'2019-06-07 05:05:00', title:'c2'),
+Coupon.create(id:4,expiring_type: 'expiring_date',discount_value: 12,discount_Type: 'Percentage',current_usage: 0,usage_num: 12, expiring_date:'2019-05-13 02:02:00',title: 'ccc'),
+Coupon.create(id:5,expiring_type: 'usage_num',discount_value: 10,discount_Type: 'Fixed',current_usage: 1,usage_num: 10,expiring_date: '2019-06-06 06:05:00',title: 'test')
 
-
+  
 # Store.create(name:"first store",summary:"We recommend Monty Python and the Holy Grail for some light-humoured dummy text.")
 # Store.create(name:"Second Store",summary:"Get ready for plenty of religious references that fail to make sense – Monty Python fans know what we mean")
 
-# Product.create(title:"Projector",image:"",price:1000,quantity_in_stock:100,description:"ready for plenty of religious references",category_id:2, brand_id:2,user_id:1)  
-# Product.create(title:"Routers",image:"",price:800,quantity_in_stock:60,description:"ready for plenty of religious references",category_id:3, brand_id:5,user_id:2)  
-# Product.create(title:"Printer",image:"",price:1000,quantity_in_stock:100,description:"ready for plenty of religious references",category_id:4, brand_id:3,user_id:1)  
+ #product1=Product.create(title:"Projector",image:"",price:1000,quantity_in_stock:100,description:"ready for plenty of religious references",category_id:2, brand_id:2,user_id:7)  
+ #product2=Product.create(title:"Routers",image:"",price:800,quantity_in_stock:60,description:"ready for plenty of religious references",category_id:3, brand_id:5,user_id:8)  
+ #product3=Product.create(title:"Printer",image:"",price:1000,quantity_in_stock:100,description:"ready for plenty of religious references",category_id:4, brand_id:3,user_id:7)  
+#product1.image.attach(io: File.open("app/assets/images/myavatar.png"), filename: "myavatar.png")
+#product2.image.attach(io: File.open("app/assets/images/avatar_nick.png"), filename: "avatar_nick.png")
+#product3.image.attach(io: File.open("app/assets/images/defaultFemalePP.png"), filename: "defaultFemalePP.png")
+
+
+
+
+#image1 = File.open("app/assets/images/myavatar.png")
+#product1.image1.attach(io: image1, filename: "myavatar.png")
+#image1.close
+#image2 = File.open("app/assets/images/avatar_nick.png")
+#product2.image2.attach(io: image2, filename: "avatar_nick.png")
+#image2.close
+#image3 = File.open("app/assets/images/defaultFemalePP.png")
+#product3.image3.attach(io: image3, filename: "defaultFemalePP.png") 
+#image3.close
